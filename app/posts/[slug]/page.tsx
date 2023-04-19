5b432cf49858ac20db6bc55ae99a0b079ed2407b
Jinnie Cho <yejincho1105@gmail.com>
2023-04-19 21:06:47 +0900

    🎨 Modify folder path in getPostContent

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,7 +4,7 @@ import matter from 'gray-matter'
 import getPostMetadata from '@/root/lib/post-metadata'
 
 const getPostContent = (slug: string) => {
-  const folder = 'posts/'
+  const folder = 'documents/posts/'
   const file = `${folder}${slug}.mdx`
   const content = fs.readFileSync(file, 'utf8')
   const matterResult = matter(content)
