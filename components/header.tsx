c8b24fc7d93bcc6f630cc6407d5e6bf528516c2e
Jinnie Cho <yejincho1105@gmail.com>
2023-04-19 22:04:37 +0900

    🎨 Separate getPageMetadata function from Header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,5 @@
-import fs from 'fs'
 import Link from 'next/link'
-import matter from 'gray-matter'
-import { PageMetadata } from '../lib/page-metadata'
-
-const getPageMetadata = (): PageMetadata[] => {
-  const folder = 'documents/pages/'
-  const files = fs.readdirSync(folder)
-  const markdownPages = files.filter((file) => file.endsWith('.mdx'))
-
-  // get gray-matter data from each page
-  const pages = markdownPages.map((fileName) => {
-    const fileContents = fs.readFileSync(`documents/pages/${fileName}`, 'utf8')
-    const matterResult = matter(fileContents)
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      slug: fileName.replace('.mdx', ''),
-    }
-  })
-
-  return pages
-}
+import getPageMetadata from '../lib/page-metadata'
 
 const Header = () => {
   const pageMetadata = getPageMetadata()
--- lib/page-metadata.ts
+++ lib/page-metadata.ts
@@ -1,5 +1,24 @@
-export interface PageMetadata {
-  title: string
-  date: string
-  slug: string
+import fs from 'fs'
+import matter from 'gray-matter'
+import { PageMetadata } from '../types/page-metadata'
+
+const getPageMetadata = (): PageMetadata[] => {
+  const folder = 'documents/pages/'
+  const files = fs.readdirSync(folder)
+  const markdownPages = files.filter((file) => file.endsWith('.mdx'))
+
+  // get gray-matter data from each page
+  const pages = markdownPages.map((fileName) => {
+    const fileContents = fs.readFileSync(`documents/pages/${fileName}`, 'utf8')
+    const matterResult = matter(fileContents)
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      slug: fileName.replace('.mdx', ''),
+    }
+  })
+
+  return pages
 }
+
+export default getPageMetadata
