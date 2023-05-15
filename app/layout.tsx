ec3046a3628e5daa8c7acf5587b6f0b543800400
Jinnie Cho <yejincho1105@gmail.com>
2023-05-16 03:25:03 +0900

    🔨 Implement mono font to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Cormorant_Garamond } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 
 import Sidebar from '../components/sidebar'
 
@@ -9,6 +10,13 @@ const cormorant_garamond = Cormorant_Garamond({
   weight: ['300', '600'],
   subsets: ['latin'],
   variable: '--font-cormorant-garamond',
+  display: 'swap',
+})
+
+const fira_code = Fira_Code({
+  weight: ['300', '600'],
+  subsets: ['latin'],
+  variable: '--font-fira-code',
   display: 'swap',
 })
 
@@ -26,7 +34,10 @@ export default function RootLayout({
   children: React.ReactNode
 }): JSX.Element {
   return (
-    <html lang="en" className={cormorant_garamond.variable}>
+    <html
+      lang="en"
+      className={`${cormorant_garamond.variable} ${fira_code.variable}`}
+    >
       <body suppressHydrationWarning={true}>
         <Sidebar />
         <main>{children}</main>
