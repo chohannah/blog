b6bdb659468088b602ca47a05b4aa5e01f43e54e
Jinnie Cho <yejincho1105@gmail.com>
2023-05-18 01:36:16 +0900

    🔥 Remove unused sidebar-wrapper from Sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -49,78 +49,76 @@ export default function Sidebar() {
   const currentYear = useMemo(() => new Date().getFullYear(), [])
 
   return (
-    <div className="col-sm-4 col-md-3 col-lg-2">
+    <div className="col-sm-4 col-md-3 col-lg-3">
       <header className="sidebar">
-        <div className="sidebar-wrapper">
-          <h1 className="sidebar-logo">
-            <Link href="/">
-              <Image
-                width={42}
-                height={42}
-                src="/images/logo.svg"
-                alt="logo of yejin's blog"
-              />
-            </Link>
-          </h1>
+        <h1 className="sidebar-logo">
+          <Link href="/">
+            <Image
+              width={42}
+              height={42}
+              src="/images/logo.svg"
+              alt="logo of yejin's blog"
+            />
+          </Link>
+        </h1>
 
-          <LayoutGroup>
-            <nav className="sidebar-nav">
-              <ul className="sidebar-nav-list">
-                {Object.entries(navItems).map(([path, { menu }]) => {
-                  const isActive = path === pathname
+        <LayoutGroup>
+          <nav className="sidebar-nav">
+            <ul className="sidebar-nav-list">
+              {Object.entries(navItems).map(([path, { menu }]) => {
+                const isActive = path === pathname
 
-                  return (
-                    <Link
-                      key={path}
-                      href={path}
-                      className={clsx({
-                        'menu-active': isActive,
-                        'menu-inactive': !isActive,
-                      })}
-                    >
-                      <li className="sidebar-nav-list-item">
-                        {menu}
-                        {path === pathname ? (
-                          <motion.div
-                            className="menu-dynamic-bg"
-                            layoutId="sidebar"
-                            transition={{
-                              type: 'spring',
-                              stiffness: 216,
-                              damping: 32,
-                            }}
-                          />
-                        ) : null}
-                      </li>
-                    </Link>
-                  )
-                })}
-              </ul>
-            </nav>
-          </LayoutGroup>
-
-          <div className="sidebar-links sm-hidden">
-            <Link className="email" href="mailto:hey.yejinc@gmail.com">
-              hey.yejinc@gmail.com
-            </Link>
-
-            <ul className="links-list">
-              {socialItems.map((item) => {
                 return (
-                  <li className="links-list-item" key={item.name}>
-                    <Link href={item.link}>
-                      {item.name}
-                      <span className="icon-arrow">
-                        <LinkArrowIcon />
-                      </span>
-                    </Link>
-                  </li>
+                  <Link
+                    key={path}
+                    href={path}
+                    className={clsx({
+                      'menu-active': isActive,
+                      'menu-inactive': !isActive,
+                    })}
+                  >
+                    <li className="sidebar-nav-list-item">
+                      {menu}
+                      {path === pathname ? (
+                        <motion.div
+                          className="menu-dynamic-bg"
+                          layoutId="sidebar"
+                          transition={{
+                            type: 'spring',
+                            stiffness: 216,
+                            damping: 32,
+                          }}
+                        />
+                      ) : null}
+                    </li>
+                  </Link>
                 )
               })}
             </ul>
+          </nav>
+        </LayoutGroup>
 
-            <p className="copyright">&copy;{currentYear} yejin cho</p>
-          </div>
+        <div className="sidebar-links sm-hidden">
+          <Link className="email" href="mailto:hey.yejinc@gmail.com">
+            hey.yejinc@gmail.com
+          </Link>
+
+          <ul className="links-list">
+            {socialItems.map((item) => {
+              return (
+                <li className="links-list-item" key={item.name}>
+                  <Link href={item.link}>
+                    {item.name}
+                    <span className="icon-arrow">
+                      <LinkArrowIcon />
+                    </span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+
+          <p className="copyright">&copy;{currentYear} yejin cho</p>
         </div>
       </header>
     </div>
