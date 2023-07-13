70eed520b4f371796177f48783ce6d990e941e47
Jinnie Cho <yejincho1105@gmail.com>
2023-07-13 19:33:33 +0900

    ✨ Implement TocPost component to blog-post-content component

--- components/blog-post-content.tsx
+++ components/blog-post-content.tsx
@@ -1,12 +1,19 @@
+'use client'
+
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import { MdxProps } from '../types/mdx'
+import TocPost from './toc-post'
+
+const mdxComponents = {
+  TocPost,
+}
 
 export const MdxRenderer = ({ code }: MdxProps) => {
   const Component = useMDXComponent(code)
 
   return (
     <article className="blog-post-content">
-      <Component />
+      <Component components={mdxComponents} />
     </article>
   )
 }
