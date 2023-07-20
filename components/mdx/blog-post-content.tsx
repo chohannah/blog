ad0611599160684bf23fa1a6a4bc6f5bdc8f95ad
Jinnie Cho <yejincho1105@gmail.com>
2023-07-21 02:16:49 +0900

    🔨 Implement CodeBlock component to blog-post-content MdxRenderer

--- components/mdx/blog-post-content.tsx
+++ components/mdx/blog-post-content.tsx
@@ -2,10 +2,12 @@
 
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import { MdxProps } from '../../types/mdx'
+import CodeBlock from './code-block'
 import Toc from './toc'
 
 const mdxComponents = {
   Toc,
+  pre: CodeBlock,
 }
 
 export const MdxRenderer = ({ code }: MdxProps) => {
