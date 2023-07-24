1f8995e0f5b1611f7ed7e90c33915f27ea17deff
Jinnie Cho <yejincho1105@gmail.com>
2023-07-24 19:29:31 +0900

    ✨ Create and style component: ReadingProgressBar

---
+++ components/mdx/reading-progress-bar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+const ReadingProgressBar = () => {
+  const [width, setWidth] = useState(0)
+
+  const scrollHeight = () => {
+    const element = document.documentElement
+    const ScrollTop = element.scrollTop || document.body.scrollTop
+    const ScrollHeight = element.scrollHeight || document.body.scrollHeight
+    const ClientHeight = element.clientHeight
+    const percent = (ScrollTop / (ScrollHeight - ClientHeight)) * 100
+
+    setWidth(percent)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHeight)
+    return () => window.removeEventListener('scroll', scrollHeight)
+  })
+
+  return (
+    <progress
+      className="reading-progress-bar"
+      value={`${width}`}
+      max={100}
+    ></progress>
+  )
+}
+
+export default ReadingProgressBar
