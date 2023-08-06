739c18a37df5c4ab1c3417d7e150423a9e12ab38
Jinnie Cho <yejincho1105@gmail.com>
2023-08-06 20:00:49 +0900

    💫 Set transition to NotFound and set button inside .not-found-guide accordingly

--- app/blog/not-found.tsx
+++ app/blog/not-found.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+
+import { motion } from 'framer-motion'
+import { fadeInHalf, staggerHalf } from '@/root/constants/animations'
 
 import { notFoundGifUrlIdList } from '@/root/constants/images'
 import LinkOutlinedArrow from '@/root/components/modules/link-outlined-arrow'
@@ -9,42 +14,52 @@ export default function NotFound() {
   const selectedGifUrl = `https://media.giphy.com/media/${notFoundGifUrlIdList[randomIndex].id}/giphy.gif`
 
   return (
-    <section className="not-found">
+    <motion.section
+      className="not-found"
+      variants={staggerHalf}
+      initial="initial"
+      animate="animate"
+    >
       <div className="container">
         <div className="row">
           <div className="col-sm-4 col-md-6 col-lg-7 sm-hidden">
-            <div className="not-found-title">
+            <motion.div className="not-found-title" variants={fadeInHalf}>
               <h2 className="not-found-title-first">404</h2>
               <h3 className="not-found-title-second">Oops! Page not found.</h3>
-            </div>
+            </motion.div>
 
-            <div className="not-found-guide">
+            <motion.div className="not-found-guide" variants={fadeInHalf}>
               <h4 className="not-found-guide-title">How about:</h4>
+
               <ol className="not-found-guide-list">
                 <li className="not-found-guide-list-item">
                   Double-check the URL for any typos or errors.
                 </li>
                 <li className="not-found-guide-list-item">
-                  Return to the <Link href="/">Homepage</Link> to explore other
-                  content.
+                  Return to the <Link href="/blog">Blog page</Link> to explore
+                  other content.
                 </li>
                 <li className="not-found-guide-list-item">
                   Use the navigation menu above to find what you&apos;re looking
                   for.
                 </li>
               </ol>
-            </div>
-            <LinkOutlinedArrow
-              className="not-found-button"
-              href="/"
-              aria-label="Go to home"
-            >
-              Back to home
-            </LinkOutlinedArrow>
+
+              <LinkOutlinedArrow
+                className="not-found-button"
+                href="/"
+                aria-label="Go to blog"
+              >
+                Back to blog
+              </LinkOutlinedArrow>
+            </motion.div>
           </div>
 
           <div className="col-sm-4 col-md-6 col-lg-5">
-            <figure className="not-found-image-group">
+            <motion.figure
+              className="not-found-image-group"
+              variants={fadeInHalf}
+            >
               <div className="image">
                 <Image
                   src={selectedGifUrl}
@@ -57,17 +72,18 @@ export default function NotFound() {
               <figcaption className="image-caption">
                 <a href={notFoundGifUrlIdList[randomIndex].url}>via GIPHY</a>
               </figcaption>
-            </figure>
+            </motion.figure>
           </div>
 
           <div className="col-sm-4 col-md-6 col-lg-7 sm-only">
-            <div className="not-found-title">
+            <motion.div className="not-found-title" variants={fadeInHalf}>
               <h2 className="not-found-title-first">404</h2>
               <h3 className="not-found-title-second">Oops! Page not found.</h3>
-            </div>
+            </motion.div>
 
-            <div className="not-found-guide">
+            <motion.div className="not-found-guide" variants={fadeInHalf}>
               <h4 className="not-found-guide-title">How about:</h4>
+
               <ol className="not-found-guide-list">
                 <li className="not-found-guide-list-item">
                   Double-check the URL for any typos or errors.
@@ -81,18 +97,19 @@ export default function NotFound() {
                   for.
                 </li>
               </ol>
-            </div>
-            <LinkOutlinedArrow
-              className="not-found-button"
-              href="/"
-              aria-label="Go to home"
-              target="_blank"
-            >
-              Back to home
-            </LinkOutlinedArrow>
+
+              <LinkOutlinedArrow
+                className="not-found-button"
+                href="/"
+                target="_blank"
+                aria-label="Go to home"
+              >
+                Back to home
+              </LinkOutlinedArrow>
+            </motion.div>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
